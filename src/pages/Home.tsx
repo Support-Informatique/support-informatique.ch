38def19af8b5b5ed56d3ab7374d45e9587186507
Elwan Mayencourt <elwan.mayencourt@gmail.com>
2023-06-09 20:17:57 +0200

    🎉 Frontend skeleton

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+// react
+
+// css
+
+// functions
+
+// components
+import Page from '../components/Page';
+
+const Home: React.FC = () => {
+    return <Page title='Home'>
+		
+	</Page>;
+};
+
+export default Home;
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,16 @@
+// react
+
+// css
+
+// functions
+
+// components
+import Page from '../components/Page';
+
+const NotFound: React.FC = () => {
+    return <Page title='Not Found'>
+		
+	</Page>;
+};
+
+export default NotFound;
