bd53ac232d608a1c2493a9c5c93851c5635de2ed
Lionel Ding <lionel.ding@hotmail.ch>
2023-06-13 20:36:55 +0200

    ✨ new 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,10 @@
 const NotFound: React.FC = () => {
-  return <h1>Not Found</h1>
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <h1 className='text-4xl font-bold'>404</h1>
+      <h2 className='text-2xl font-bold'>How did you end up here?</h2>
+    </div>
+  )
 }
 
 export default NotFound
