0a1296859d008e6d14f316712a2296add3921fdc
Lionel Ding <lionel.ding@hotmail.ch>
2023-06-13 20:47:25 +0200

    💄 improve 404 page and add .htaccess

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,21 @@
+// components
+import Button from '@/components/utils/Button'
+
 const NotFound: React.FC = () => {
+  const goBackHome = () => {
+    window.location.href = '/'
+  }
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <h1 className='text-4xl font-bold'>404</h1>
-      <h2 className='text-2xl font-bold'>How did you end up here?</h2>
+      <h2 className='text-2xl font-bold mb-5'>How did you end up here?</h2>
+      <Button
+        size='sm'
+        type='outlined'
+        text='Go back home'
+        onClick={goBackHome}
+      />
     </div>
   )
 }
