1cfe22d9c69ed2d89be5603ce18db59d67ad6d88
Lionel Ding <lionel.ding@studentfr.ch>
2023-06-16 11:54:04 +0200

    ✏️ format config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
         darkText: '#011430',
         lightText: '#01143080',
         accentText: '#011430BF',
-        background: "var(--background)",
+        background: 'var(--background)',
         lightCard: '#B7EFFF1F'
       },
       fontWeight: {
